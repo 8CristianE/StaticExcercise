73c3fda334de3f971983dbd4f56cc1b7596b5279
cristianespinosa7621@gmail.com <email@example.com>
2022-06-20 15:23:00 -0700

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("What temperature in Fahrenhiet do you want to convert to Celsius");
+            double ftemp = Convert.ToDouble(Console.ReadLine());
+            double ctemp = TempConverter.FahrenheitToCelsius(ftemp);
+            Console.WriteLine($"{ctemp} degrees Celsuius");
+            ;
+            Console.WriteLine("**************************");
+
+            Console.WriteLine("What temperature in Celsuis do you want to convert to Fahrenhiet");
+            double Ctemp = Convert.ToDouble(Console.ReadLine());
+            double Ftemp = TempConverter.FahrenheitToCelsius(Ctemp);
+            Console.WriteLine($"{Ftemp} degrees Fahrenhiet");
+
+
+        }
+
+    }
+}
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FahrenheitToCelsius(double fnum)
+		{
+			var Celsius = (fnum - 32) * 5 / 9;
+			return Celsius;
+		}
+
+		public static double CelsiusToFahrenheit(double cnum)
+        {
+
+			return (cnum * (9 / 5)) + 32;
+        }
+
+        
+    }
+}
+
+
